fd6722db65ec0e936838320c273efc7c7555fe39
Courage <dieter.dieter.no1@gmail.com>
2024-03-13 21:15:34 +0200

    Update clear-channel.py

--- clear-channel.py
+++ clear-channel.py
@@ -14,9 +14,9 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
     # Replace 'SERVER_ID' and 'CHANNEL_ID' with your server and channel IDs
-    server_id = 'SERVER_ID'
-    channel_id = CHANNEL_ID
-    
+    server_id = '1186820354186948749'
+    channel_id = 1212669690678349824
+
     # Fetch the server and channel
     server = bot.get_guild(int(server_id))
     channel = server.get_channel(int(channel_id))
@@ -24,7 +24,7 @@ async def on_ready():
     # Fetch messages and delete them with a delay
     async for message in channel.history(limit=None):
         await message.delete()
-        await asyncio.sleep(0.8)  # Add a delay of 0.8 seconds between deletions
+        await asyncio.sleep(1.2)  # Add a delay of 0.8 seconds between deletions
 
     print(f'All messages in #{channel.name} have been deleted.')
     await bot.close()  # Close the bot after all the messages have been deleted
